592663ed54090ab25e3304521a3f3aff0ffcf6e6
Maksim Gavr <gavrilenk0.maksimik@ya.ru>
2024-09-02 20:05:16 +0700

    first commit api

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -2,6 +2,34 @@
  * Основная функция для совершения запросов
  * на сервер.
  * */
-const createRequest = (options = {}) => {
 
+const createRequest = (options = {}) => {
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+  let xhrUrl = options.url;
+  let formData = null;
+  
+  if (options.method === 'GET' && options.data) {
+    xhrUrl += '?';
+    Object.entries(options.data).forEach(([key, value]) => xhrUrl += `${key}=${value}&`);
+    xhrUrl = xhrUrl.slice(0, -1);
+  } else {
+    if (options.data) {
+      formData = new FormData();
+      Object.entries(options.data).forEach(([key, value]) => formData.append(key, value));
+    }
+  }
+
+  xhr.onload = () => {
+    if (xhr.status >= 200 && xhr.status < 300) {
+      options.callback(null, xhr.response);
+    }
+  }
+
+  try {
+    xhr.open(options.method, xhrUrl);
+    xhr.send(formData);
+  } catch (err) {
+    options.callback(err);
+  }
 };
