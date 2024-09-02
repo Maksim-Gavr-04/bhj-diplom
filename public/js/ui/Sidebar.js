e5d70baf740954447ab31fefa9a6b2e636c534ae
Maksim Gavr <gavrilenk0.maksimik@ya.ru>
2024-09-02 20:16:10 +0700

    first commit Sidebar

--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -3,6 +3,7 @@
  * кнопки скрытия/показа колонки в мобильной версии сайта
  * и за кнопки меню
  * */
+
 class Sidebar {
   /**
    * Запускает initAuthLinks и initToggleButton
@@ -18,17 +19,41 @@ class Sidebar {
    * при нажатии на кнопку .sidebar-toggle
    * */
   static initToggleButton() {
+    const miniSidebar = document.querySelector('.sidebar-mini');
+    const sidebarToggle = document.querySelector('.sidebar-toggle');
 
+    sidebarToggle.addEventListener('click', () => {
+      miniSidebar.classList.toggle('sidebar-open');
+      miniSidebar.classList.toggle('sidebar-collapse');
+    });
   }
 
   /**
    * При нажатии на кнопку входа, показывает окно входа
-   * (через найденное в App.getModal)
-   * При нажатии на кнопку регастрации показывает окно регистрации
+   * (через найденное в App.getModal).
+   * При нажатии на кнопку регистрации показывает окно регистрации.
    * При нажатии на кнопку выхода вызывает User.logout и по успешному
    * выходу устанавливает App.setState( 'init' )
    * */
   static initAuthLinks() {
+    const registerButton = document.querySelector('.menu-item_register');
+    const loginButton = document.querySelector('.menu-item_login');
+    const logoutButton = document.querySelector('.menu-item_logout');
+    
+    registerButton.addEventListener('click', () => {
+      const registerModal = App.getModal('register');
+      registerModal.open();
+    });
 
+    loginButton.addEventListener('click', () => {
+      const loginModal = App.getModal('login');
+      loginModal.open();
+    });
+
+    logoutButton.addEventListener('click', () => {
+      User.logout((err, response) => {
+        if (response.success) App.setState('init');
+      });
+    });
   }
 }
