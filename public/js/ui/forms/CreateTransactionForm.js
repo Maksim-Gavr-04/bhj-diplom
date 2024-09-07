3a7aa636b117153ee8f24f143bf11f1db31f5231
Maksim Gavr <gavrilenk0.maksimik@ya.ru>
2024-09-07 15:23:32 +0700

    first commit CreateTransactionsForm

--- public/js/ui/forms/CreateTransactionForm.js
+++ public/js/ui/forms/CreateTransactionForm.js
@@ -2,21 +2,37 @@
  * Класс CreateTransactionForm управляет формой
  * создания новой транзакции
  * */
+
 class CreateTransactionForm extends AsyncForm {
   /**
    * Вызывает родительский конструктор и
    * метод renderAccountsList
    * */
   constructor(element) {
-    super(element)
+    super(element);
+    this.renderAccountsList();
   }
 
   /**
-   * Получает список счетов с помощью Account.list
+   * Получает список счетов с помощью Account.list.
    * Обновляет в форме всплывающего окна выпадающий список
    * */
   renderAccountsList() {
+    Account.list(null, (err, response) => {
+      if (response.success) {
+        const arrOfAccountObjects = [...response.data];
 
+        const incomeAccountsList = document.getElementById('income-accounts-list');
+        incomeAccountsList.innerHTML = '';
+        const expenseAccountsList = document.getElementById('expense-accounts-list');
+        expenseAccountsList.innerHTML = '';
+        
+        arrOfAccountObjects.forEach(object => {
+          incomeAccountsList.innerHTML += `<option value="${object.id}">${object.name}</option>`;
+          expenseAccountsList.innerHTML += `<option value="${object.id}">${object.name}</option>`;
+        });
+      }
+    });
   }
 
   /**
@@ -26,6 +42,16 @@ class CreateTransactionForm extends AsyncForm {
    * в котором находится форма
    * */
   onSubmit(data) {
+    Transaction.create(data, (err, response) => {
+      if (response.success) {
+        this.element.reset();
+        const incomeModal = App.getModal('newIncome');
+        incomeModal.close();
 
+        const expenseModal = App.getModal('newExpense');
+        expenseModal.close();
+        App.update();
+      }
+    });
   }
 }
